3d0392e332e69e31edf2e5d1c70dc8f827420296
Reinaldo Ospino <reinaldo.aospino@gmail.com>
2020-05-16 19:06:07 -0300

    Updates

---
+++ precache-manifest.26d748875e3c93acde68544803d55ff8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f87bf3adc24198d46af2fc15bd5164",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a95830b571f9c697875f",
+    "url": "/monsters-rolodex/static/css/main.4674e284.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a95830b571f9c697875f",
+    "url": "/monsters-rolodex/static/js/main.2290bb7e.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
